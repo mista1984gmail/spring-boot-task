92f7637f5f1411e8c3e9ae75cc6c7ed6c2325067
s.mitskevich <mista1984@gmail.com>
2024-01-25 22:51:40 +0300

    Initial commit

---
+++ src/test/java/ru/clevertec/springboottask/SpringBootTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.clevertec.springboottask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
